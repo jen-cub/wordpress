92866a74e336792142d68efbf773d3f6b342be70
jen-cub <36495714+jen-cub@users.noreply.github.com>
2018-02-21 20:15:55 +1100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'Test1234!');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', ':/cloudsql/jenwp-195908:australia-southeast1:jencldsql');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
